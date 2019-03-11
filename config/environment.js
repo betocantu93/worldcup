84bcbb2e27d678ed381eb710bc2b04b514cee0ad
Beto Cantu <betocantu93@gmail.com>
2019-03-10 18:23:50 -0600

    added fastboot and prember

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,10 @@ module.exports = function(environment) {
       }
     },
 
+    fastboot: {
+      hostWhitelist: [/^localhost:\d+$/]
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
--- src/ui/routes/index/route.js
+++ src/ui/routes/index/route.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 
 const api = "https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json";
+import fetch from 'fetch';
 
 export default class IndexRoute extends Route {
 	async model() {
